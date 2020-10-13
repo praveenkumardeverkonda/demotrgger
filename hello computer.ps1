739e1a5b86b2f347be11adb0742ae2de4bf07c78
=praveen kumar deverkonda <praveen>
2020-10-13 09:39:09 +0530

    first commit

---
+++ hello computer.ps1
@@ -0,0 +1,9 @@
+﻿#$firstname = $args[0]
+#$secondname = $args[1]
+
+param($firstname,$secondname)
+
+Write-Host "hello world $firstname $secondname"
+#Write-Host "hello $firstname $secondname"
+write-host "the computer name is $env:ComSpec"
+
